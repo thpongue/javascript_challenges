cd5498b5d93474c6be30d9a07ae6e91d4f692fc1
Dominic McAdden <dominic.mcadden@gmail.com>
2015-01-22 22:54:01 +0000

    committed failing test for fizz buzz - this is my personal favourite drinking game how have I never written code for this before\!

---
+++ spec/test2.js
@@ -0,0 +1,10 @@
+/*global describe, it */
+'use strict';
+
+(function () {
+describe('Implement FizzBuzz - I\'ve always wanted to do this!', function () {
+	it('should count up to ten', function () {
+			expect(fizzBuzz(10)).toEqual(["1", "2", "fizz", "four", "five", "fizz", "buzz", "8", "fizz", 10]);
+		});
+	});
+})();
