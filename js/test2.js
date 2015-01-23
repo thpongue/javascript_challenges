146ad7d5ebd99b0deb3dcf771169b8cdb755c90c
Dominic McAdden <dominic.mcadden@gmail.com>
2015-01-23 11:25:00 +0000

    Using nested functions to avoid passing the value around

--- js/test2.js
+++ js/test2.js
@@ -1,31 +1,33 @@
 function fizzBuzz(val) {
+
+	function isFizz() {
+		return isDivisibleBy(3) || includesNumber(3);
+	}
+
+	function isBuzz() {
+		return isDivisibleBy(7) || includesNumber(7);
+	}
+
+	function isDivisibleBy(divisibleBy) {
+		return (val % divisibleBy === 0); 
+	}
+
+	function includesNumber(includesNumber) {
+		return val.toString().indexOf(includesNumber)>-1; 
+	}
+
 	var ret = '';
-	if (isFizz(val)) {
+	if (isFizz()) {
 		ret = 'fizz';
 	}
 
-	if (isBuzz(val)) {
+	if (isBuzz()) {
 		ret += 'buzz';
 	}
 
 	if (!ret) {
 		ret = val.toString();
 	}
+
 	return ret;
-}
-
-function isFizz(val) {
-	return isDivisibleBy(val,3) || includesNumber(val, 3);
-}
-
-function isBuzz(val) {
-	return isDivisibleBy(val,7) || includesNumber(val, 7);
-}
-
-function isDivisibleBy(val, divisibleBy) {
-	return (val % divisibleBy === 0); 
-}
-
-function includesNumber(val, includesNumber) {
-	return val.toString().indexOf(includesNumber)>-1; 
 }
