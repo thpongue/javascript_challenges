29dcb6fdd1b15608fc1d81ad0e545ab7abeec275
Dominic McAdden <dominic.mcadden@gmail.com>
2015-01-23 12:32:23 +0000

    Added the first failing test - now to write some proper code!

--- spec/test3.js
+++ spec/test3.js
@@ -1,12 +1,19 @@
 /*global describe, it */
 'use strict';
 
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,
+//144, 233, 377, 610, 987, 1597, 2584, 4181,
+//6765, 10946, 17711, 28657, 46368, 75025,
+//121393, 196418, 317811
+
 (function () {
 	describe('Return a boolean indicatin if a particular number is part of a fibonacci sequence', function () {
-		it('should count up to 20 returning a boolean to indicate if the number is in the fibonacci sequence', function () {
+		it('should count up to 21 returning a boolean to indicate if the number is in the fibonacci sequence', function () {
 			expect(fibonacciSequence(0)).toBe(true);
-//			expect(fibonacciSequence(1)).toBe(true);
-//			expect(fibonacciSequence(2)).toBe(false);
+			expect(fibonacciSequence(1)).toBe(true);
+			expect(fibonacciSequence(2)).toBe(true);
+			expect(fibonacciSequence(3)).toBe(true);
+			expect(fibonacciSequence(4)).toBe(false);
 		});
 	});
 })();
